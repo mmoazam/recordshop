816c9e6228d5b7d302ac462f3b6e279a08a48e6e
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-07 14:01:34 +0100

    add Album, Artist, Genre

---
+++ src/main/java/com/northcoders/recordshop/model/Artist.java
@@ -0,0 +1,16 @@
+package com.northcoders.recordshop.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Artist {
+
+}
---
+++ src/main/java/com/northcoders/recordshop/model/Genre.java
@@ -0,0 +1,24 @@
+package com.northcoders.recordshop.model;
+
+
+public enum Genre {
+    POP,
+    ROCK,
+    JAZZ,
+    CLASSICAL,
+    METAL,
+    FOLK,
+    COUNTRY,
+    BLUES,
+    RAP,
+    RNB,
+    HIPHOP,
+    ELECTRONIC,
+    INSTRUMENTAL,
+    ALTERNATIVE,
+    REGGAE,
+    SOUL,
+    PUNK,
+    NEWAGE,
+    DISCO
+}
