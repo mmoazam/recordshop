f0081d42d81069af6de05ecf52c05f1ff917753d
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-10 10:52:13 +0100

    add actuator health to application.properties check health localhost:8080/actuator/health

--- src/main/java/com/northcoders/recordshop/RecordShopApplication.java
+++ src/main/java/com/northcoders/recordshop/RecordShopApplication.java
@@ -20,10 +20,12 @@ public class RecordShopApplication {
 	@Bean
 	CommandLineRunner runner(IAlbumRepository albumRepository) {
 		return args -> {
-			albumRepository.save(new Album(1L,"Album 1", "Album 1 description", "Artist 1", Genre.ROCK, 2020, 10));
+			albumRepository.save(new Album(1L,"Album 1", "Album 1 description", "Artist 1", Genre.ROCK, 2020, 20));
 			albumRepository.save(new Album(2L,"Album 2", "Album 2 description", "Artist 2", Genre.POP, 2010, 10));
-			albumRepository.save(new Album(3L,"Album 3", "Album 3 description", "Artist 3", Genre.COUNTRY, 2000, 10));
-			albumRepository.save(new Album(4L,"Album 4", "Album 4 description", "Artist 4", Genre.BLUES, 1990, 10));
+			albumRepository.save(new Album(3L,"Album 3", "Album 3 description", "Artist 3", Genre.COUNTRY, 2000, 5));
+			albumRepository.save(new Album(4L,"Album 4", "Album 4 description", "Artist 4", Genre.BLUES, 1990, 16));
+			albumRepository.save(new Album(5L,"Country 5", "Mountains and Trees", "JOhn Denver", Genre.COUNTRY, 1980, 1));
+			albumRepository.save(new Album(6L,"Rubbish Vol 7", "Music of the 2000 and beyond", "All of them", Genre.HIPHOP, 2000, 100));
 		};
 	}
 }// end of class
