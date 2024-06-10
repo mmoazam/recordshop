997b07c24d5448c3f0c83725a7965672530457c6
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-10 17:20:36 +0100

    add constraints to Album.java. Artist and Album name combination must be unique. update POM.xml to import org.hibernate.validator to add required annotations.

--- src/main/java/com/northcoders/recordshop/model/Album.java
+++ src/main/java/com/northcoders/recordshop/model/Album.java
@@ -1,6 +1,9 @@
 package com.northcoders.recordshop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 
@@ -10,24 +13,32 @@ import lombok.*;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Table(name = "albums")
+@ToString
+@Table(name = "albums", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"name","artist"})
+})
 public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(unique = true)
+    @NotNull(message = "Name cannot be null")
+    @NotBlank(message = "Album name is mandatory")
     private String name;
 
     private String description;
 
+    @NotNull(message = "Artist cannot be null")
+    @NotBlank(message = "Artist name is mandatory")
     private String artist;
 
     @Enumerated(EnumType.STRING)
     private Genre genre;
 
+    @NotNull(message = "Genre is mandatory")
     private int releaseYear;
 
+    @Min(value = 0, message = "Stock level cannot be less than 0")
     private int stockLevel;
 
 }
