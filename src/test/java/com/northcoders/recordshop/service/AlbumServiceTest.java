ecd0721eb101a8c94ab62f804484d82bd1059719
Mister <14061746+mmoazam@users.noreply.github.com>
2024-06-13 20:42:34 +0100

    impl: AlbumServiceTest addOrUpdateAlbum

--- src/test/java/com/northcoders/recordshop/service/AlbumServiceTest.java
+++ src/test/java/com/northcoders/recordshop/service/AlbumServiceTest.java
@@ -247,9 +247,20 @@ class AlbumServiceTest {
 
     @Test
     void addOrUpdateAlbum() {
+        Album album = new Album();
+        album.setId(1L);
+        album.setName("Album 1");
+        album.setArtist("Artist 1");
+        album.setStockLevel(10);
+        album.setReleaseYear(2000);
+        album.setDescription("Album 1 description");
+        album.setGenre(Genre.ROCK);
+        albumService.addOrUpdateAlbum(album);
+
+
+        album.setName("Album 2");
+        albumService.addOrUpdateAlbum(album);
+        verify(albumRepository, times(2)).save(album);
     }
 
-    @Test
-    void testUpdateAlbum() {
-    }
 }
